16926be47238cb4a5471b9c10c6a12d79fd66e9c
Ayush Singh <56454838+ayushs1ngh@users.noreply.github.com>
2021-10-04 13:39:31 +0530

    Executing commands in java
    commit

---
+++ Other Programs/ExecutingCommands.java
@@ -0,0 +1,14 @@
+/*
+xecuting Comments: Most of the developers think comments are never executed in a program and are used for ease in understanding the code. But, they are executed.
+This comment is executed because of Unicode character “\u000d” and java compiler parses this unicode character as a new line. Java allows using Unicode characters without encoding.
+*/
+
+public class ExecutingCommands {
+	public static void main(String[] args)
+	{
+		// \u000d System.out.println("HactoberFest 2021");
+	}
+}
+
+
+Output: HacktoberFest 2021
